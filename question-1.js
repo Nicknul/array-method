753f20aa9abf8e472a0c1fbc1d43d0d7268d7902
Nicknul <jea7730@gmail.com>
2024-06-26 12:37:30 +0900

    filter

--- question-1.js
+++ question-1.js
@@ -4,3 +4,18 @@ const item = [
   { name: '물', stock: 10 },
   { name: '주스', stock: 0 },
 ];
+
+//재고가 있는 상품만 나열하도록 수정
+// ? filter 사용
+const availableItem1 = item.filter((item) => {
+  return item.stock > 0;
+}); // [ { name: '콜라', stock: 5 }, { name: '물', stock: 10 } ]
+
+// const availableItem2 = item.filter((item) => {
+//   return item.stock > 0;
+// });
+// const availableItem3 = item.filter((item) => {
+//   return item.stock > 0;
+// });
+
+console.log(availableItem1);
