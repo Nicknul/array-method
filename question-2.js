855bb189e613ee004b3961588ee2dc02cd0a7752
Nicknul <jea7730@gmail.com>
2024-06-26 12:42:52 +0900

    문제 2: 상품 가격 리스트 생성 > map()

--- question-2.js
+++ question-2.js
@@ -4,3 +4,13 @@ const item = [
   { name: '물', price: 800 },
   { name: '주스', price: 1500 },
 ];
+
+// forEach가 안되는 이유 -> forEach 검색할 것
+const itemList = item.forEach((item) => {
+  return `${item.name} : ${item.price}원`;
+});
+// const itemList = item.map((item) => {
+//   return `${item.name} : ${item.price}원`;
+// });
+
+console.log(itemList); // [ '콜라 : 1000원', '사이다 : 1200원', '물 : 800원', '주스 : 1500원' ]
